239c685c68854b1588be3af2d01c44dac3a49412
Victor <victorgmassimi.microverse@gmail.com>
2023-05-01 16:38:44 -0600

    partially renders view,  App component not showing

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,10 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+// import "@hotwired/turbo-rails"
+// import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+import App from './components/App'
 
 ReactDOM.render(
   <App/>,
---
+++ app/javascript/components/greeting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Greeting(greeting = 'Hello') {
+  return (
+    <div>{ greeting }</div>
+  )
+}
+
+export default Greeting
